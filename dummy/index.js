68bf1c652061e66c341cd60baaeaec9c2323cad6
Brahmam-12 <98756307+Brahmam-12@users.noreply.github.com>
2024-09-11 16:22:19 +0530

    Add files via upload

---
+++ dummy/index.js
@@ -0,0 +1,12 @@
+const express = require("express")
+const app = express()
+
+app.use(express.json())
+
+app.use('/', ((req, res) =>{
+    res.send("Hello World")
+}))
+
+app.listen(5050, ()=>{
+    console.log("server is running")
+})
